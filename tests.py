5a1d5b307dc5b1612240eedcfff06fc2d82abdf1
Leonardo Gomes <leonardo.gomes@lit.ifce.edu.br>
2019-02-05 18:32:28 -0300

    comit no acaizinho

---
+++ tests.py
@@ -0,0 +1,16 @@
+v = [3,6,1,9,0,4,5,8,7,2]
+
+h = 5
+
+####versão pingo
+##for i in range(h, 0, -1):
+##        if(i == 1):
+##                print("Ordenar vetor")
+##        else:
+##                for k in range(h):
+##                        lista = []
+##                        print("#########",i,"#########",k,"#########")
+##                        for j in range(k, len(v), i):
+##                                lista.append(j)
+##                        print(lista)
+
